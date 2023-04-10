8d0da1522760b3ac727eb53b791966ccca7fdc42
psyark <psyark@gmail.com>
2023-04-10 12:20:31 +0900

    コード整理

--- doc2api/methoddoc/tools.converter.go
+++ doc2api/methoddoc/tools.converter.go
@@ -82,7 +82,7 @@ func (c *converter) convert() error {
 
 func (c *converter) output(file *jen.File, doc ssrPropsDoc) error {
 	file.Comment(doc.Title).Line().Comment(c.url)
-	hasBodyParams := len(doc.API.Params.filter("body")) != 0
+	hasBodyParams := len(c.localCopyOfBodyParams) != 0
 
 	methodName := strcase.UpperCamelCase(strings.ReplaceAll(doc.Title, " a ", " "))
 	methodParams := []jen.Code{
@@ -97,34 +97,21 @@ func (c *converter) output(file *jen.File, doc ssrPropsDoc) error {
 
 		methodParams = append(methodParams, jen.Id(param.Name).Add(param.typeCode))
 	}
+
 	// オプション構造体引数
-	if hasBodyParams {
-		methodParams = append(methodParams, jen.Id("params").Op("*").Id(methodName+"Params"))
-	}
-	methodParams = append(methodParams, jen.Id("options").Op("...").Id("callOption"))
-
-	pathParams := []jen.Code{}
-	path := regexp.MustCompile(`\{\w+\}`).ReplaceAllStringFunc(doc.API.URL, func(s string) string {
-		pathParams = append(pathParams, jen.Id(s[1:len(s)-1]))
-		return "%v"
-	})
-	pathParams = append([]jen.Code{jen.Lit(path)}, pathParams...)
-
 	params := jen.Nil()
 	if hasBodyParams {
 		params = jen.Id("params")
+		methodParams = append(methodParams, jen.Id("params").Op("*").Id(methodName+"Params"))
 	}
 
-	pathCode := jen.Lit(path)
-	if len(pathParams) != 1 {
-		pathCode = jen.Qual("fmt", "Sprintf").Call(pathParams...)
-	}
+	methodParams = append(methodParams, jen.Id("options").Op("...").Id("callOption"))
 
 	file.Func().Params(jen.Id("c").Op("*").Id("Client")).Id(methodName).Params(methodParams...).Params(c.returnType.returnType(), jen.Error()).Block(
 		jen.Id("result").Op(":=").Add(c.returnType.unmarshaller()).Values(),
 		jen.Id("co").Op(":=").Op("&").Id("callOptions").Values(jen.Dict{
 			jen.Id("method"): jen.Qual("net/http", "Method"+strcase.UpperCamelCase(doc.API.Method)),
-			jen.Id("path"):   pathCode,
+			jen.Id("path"):   c.pathCode(doc),
 			jen.Id("params"): params,
 			jen.Id("result"): jen.Id("result"),
 		}),
@@ -149,6 +136,23 @@ func (c *converter) output(file *jen.File, doc ssrPropsDoc) error {
 	}
 
 	return nil
+}
+
+func (c *converter) pathCode(doc ssrPropsDoc) jen.Code {
+	pathParams := []jen.Code{}
+	pathFormat := regexp.MustCompile(`\{\w+\}`).ReplaceAllStringFunc(doc.API.URL, func(s string) string {
+		pathParams = append(pathParams, jen.Id(s[1:len(s)-1]))
+		return "%v"
+	})
+
+	// パスパラメータが無いのでURLをそのままリテラルにする
+	if len(pathParams) == 0 {
+		return jen.Lit(doc.API.URL)
+	}
+
+	// パラメータの先頭にフォーマットを追加
+	pathParams = append([]jen.Code{jen.Lit(pathFormat)}, pathParams...)
+	return jen.Qual("fmt", "Sprintf").Call(pathParams...)
 }
 
 var registeredConverters []converter
