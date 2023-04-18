30ed4a12e46dc6773b312699e5fd078afe18bb5f
psyark <psyark@gmail.com>
2023-04-18 13:41:19 +0900

    テストの整理

--- client_test.go
+++ client_test.go
@@ -17,10 +17,11 @@ import (
 var cli *Client
 
 var (
-	STANDALONE_PAGE        = uuid.MustParse("b05213d5c3af4de6924cc9b106ae93ec") // https://www.notion.so/Page-b05213d5c3af4de6924cc9b106ae93ec
-	DATABASE               = uuid.MustParse("edd0404128004a83bd29deb729221ec7") // https://www.notion.so/edd0404128004a83bd29deb729221ec7
-	DATABASE_PAGE_READONLY = uuid.MustParse("7e01d5af9d0e4d2584e4d5bfc39b65bf") // https://www.notion.so/ABCDEFG-7e01d5af9d0e4d2584e4d5bfc39b65bf
-	DATABASE_PAGE_WRITABLE = uuid.MustParse("b8ff7c186ef2416cb9654daf0d7aa961") // https://www.notion.so/PageToUpdate-b8ff7c186ef2416cb9654daf0d7aa961
+	STANDALONE_PAGE         = uuid.MustParse("b05213d5c3af4de6924cc9b106ae93ec") // https://www.notion.so/Page-b05213d5c3af4de6924cc9b106ae93ec
+	DATABASE                = uuid.MustParse("edd0404128004a83bd29deb729221ec7") // https://www.notion.so/edd0404128004a83bd29deb729221ec7
+	DATABASE_PAGE_FOR_READ1 = uuid.MustParse("7e01d5af9d0e4d2584e4d5bfc39b65bf") // https://www.notion.so/ABCDEFG-7e01d5af9d0e4d2584e4d5bfc39b65bf
+	DATABASE_PAGE_FOR_READ2 = uuid.MustParse("7e1105bc19a64a1381453cff0b488092") // https://www.notion.so/7e1105bc19a64a1381453cff0b488092
+	DATABASE_PAGE_FOR_WRITE = uuid.MustParse("b8ff7c186ef2416cb9654daf0d7aa961") // https://www.notion.so/PageToUpdate-b8ff7c186ef2416cb9654daf0d7aa961
 )
 
 func init() {
@@ -39,13 +40,13 @@ func TestRetrievePage(t *testing.T) {
 
 func TestRetrievePagePropertyItem(t *testing.T) {
 	ctx := context.Background()
-	if db, err := cli.RetrieveDatabase(ctx, DATABASE); err != nil {
+	if db, err := cli.RetrieveDatabase(ctx, DATABASE, requestId("RetrieveDatabase"), useCache(), validateResult()); err != nil {
 		t.Fatal(err)
 	} else {
 		for name, prop := range db.Properties {
 			name, prop := name, prop
 			t.Run(name, func(t *testing.T) {
-				if _, err := cli.RetrievePagePropertyItem(ctx, DATABASE_PAGE_READONLY, prop.GetId(), requestId("RetrievePagePropertyItem_"+name), useCache(), validateResult()); err != nil {
+				if _, err := cli.RetrievePagePropertyItem(ctx, DATABASE_PAGE_FOR_READ1, prop.GetId(), requestId("RetrievePagePropertyItem_"+name), useCache(), validateResult()); err != nil {
 					t.Fatal(err)
 				}
 			})
@@ -77,7 +78,7 @@ func TestUpdatePage(t *testing.T) {
 	for i, params := range cases {
 		params := params
 		t.Run(fmt.Sprintf("#%v", i), func(t *testing.T) {
-			if _, err := cli.UpdatePage(ctx, DATABASE_PAGE_WRITABLE, params, requestId("UpdatePage"), useCache(), validateResult()); err != nil {
+			if _, err := cli.UpdatePage(ctx, DATABASE_PAGE_FOR_WRITE, params, requestId("UpdatePage"), useCache(), validateResult()); err != nil {
 				x, _ := json.MarshalIndent(params, "", "  ")
 				fmt.Println(string(x))
 				t.Fatal(err)
@@ -89,13 +90,6 @@ func TestUpdatePage(t *testing.T) {
 func TestQueryDatabase(t *testing.T) {
 	ctx := context.Background()
 	if _, err := cli.QueryDatabase(ctx, DATABASE, &QueryDatabaseParams{}, requestId("QueryDatabase"), useCache(), validateResult()); err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestRetrieveDatabase(t *testing.T) {
-	ctx := context.Background()
-	if _, err := cli.RetrieveDatabase(ctx, DATABASE, requestId("RetrieveDatabase"), useCache(), validateResult()); err != nil {
 		t.Fatal(err)
 	}
 }
